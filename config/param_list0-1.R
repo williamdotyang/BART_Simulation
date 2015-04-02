b8534f3f71cf51bdab957c7d99dee852c46f5541
williamdotyang <williamdotyang@gmail.com>
2015-04-02 11:15:58 -0500

    Renamed for future convenience

--- config/param_list0-1.R
+++ config/param_list0-1.R
@@ -4,32 +4,32 @@
 ## of each parameter.
 ################################################################################
 #treatment label
-TREATMENT_LABEL = 1
+TREATMENT_LABEL = 0
 #number of covariates
 n0 = 3 
 #number of observations
-N0 = 700
+N0 = 500 
 
 ### make up intercept coefficients
-BETA0 = 200
+BETA0 = 0
 
 ### make up coefficients for covariates x1,..., xn
-BETA1 = c(1, 3, 7)
+BETA1 = c(2, 2, 2)
 ### make up 2nd order interaction index dataframe for x's, and their coefficients
 #INTER should be the same for all treatment_lable
-INTER2 = data.frame(first=c(1,2,3), second=c(2,3,1))
-BETA2 = c(-2, 5, -8)
+INTER2 = data.frame(first=c(1,2,3), second=c(3,2,1))
+BETA2 = c(1, 1, 1)
 ### make up 3nd order interaction index dataframe for x's, and their coefficients
 #INTER should be the same for all treatment_lable
 INTER3 = data.frame(first=1, second=2, third=3)
-BETA3 = 5
+BETA3 = 1
 
 ### means
 #should be the same for all treatment_lable
-MUS = c(2, 3, 4)
+MUS = c(2, 2, 2)
 
 ### specify the covariance matrix
 #should be the same for all treatment_lable
-SIGMA = matrix(nrow=n0, c(9, 2, 1, 
-                          2, 1, 0.4, 
-                          1, 0.4, 1))
+SIGMA = matrix(nrow=n0, c(1, 0, 0, 
+                          0, 1, 0, 
+                          0, 0, 1))
