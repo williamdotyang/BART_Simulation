6985f477c6dfb18a248b85af455fdc9498fab141
williamdotyang <williamdotyang@gmail.com>
2015-04-02 16:44:42 -0500

    Implemented the default plotting dimension.

--- src/plotting.R
+++ src/plotting.R
@@ -41,13 +41,22 @@ plot_data_frame = function(dataset, var_name) {
 # Calls get_plot() for covariates X_1 to X_givenIndex. Plots different X's vs. Y.
 #
 # @param dataset The data.frame to be plotted, containing Z, X's and Y.
-# @param max_n The maximun index to be plotted
-# @param plot_dim A vector indicating the plotting dimension.
+# @param max_n The maximun index to be plotted. If default, plot all.
+# @param plot_dim A vector indicating the plotting dimension. If default, plot all.
 #
 # @return No return.
 ###
-data_frame_plots = function(dataset, max_n, plot_dim) {
-  par(mfrow=plot_dim)
+data_frame_plots = function(dataset, max_n=NA, plot_dim=NA) {
+  # decide the plotting dimension
+  if (is.na(max_n) & is.na(plot_dim)) {
+    max_n = length(dataset[1, ]) - 2
+    r = ceiling(sqrt(max_n))
+    c = ceiling(max_n / r)
+    par(mfrow=c(r, c))
+  } else {
+    par(mfrow=plot_dim)
+  }
+  
   var_names = names(dataset)[2:(length(names(dataset)) - 1)]
   
   for (i in 1:max_n) {
