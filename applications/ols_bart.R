dd5fde083b5406689fc9555fd26cc5c4cefe0f99
williamdotyang <williamdotyang@gmail.com>
2015-04-03 09:37:38 -0500

    bug fix

--- applications/ols_bart.R
+++ applications/ols_bart.R
@@ -48,18 +48,19 @@ for (label in labels) {
   Y_train_label = Y_train[Z_train==label]
   X_test_label = X_test[Z_test==label, ]
   Y_test_label = Y_test[Z_test==label]
-  data_train_label = data_train[Z_train==label, ]
-  data_test_label = data_test[Z_test==label, ]
+  #data_train_label = data_train[Z_train==label, ]
+  #data_test_label = data_test[Z_test==label, ]
   
   # fitting
-  ols_fit_label = lm(Y_train_label ~ ., data=data_train_label)
-  Y_hat_ols_label = predict(ols_fit_label, data.frame(Z=Z_test, X_test_label))
+  ols_fit_label = lm(Y ~ ., data=data.frame(X_train_label, Y=Y_train_label))
+  Y_hat_ols_label = predict(ols_fit_label,  X_test_label)
   error_ols_label = Y_test_label - Y_hat_ols_label
   
-  bart_obj_label = bart(x.train=data.frame(Z=label, X_train_label), 
+  bart_obj_label = bart(x.train=X_train_label, 
                         y.train=Y_train_label, 
-                        x.test=data.frame(Z=Z_test, X_test))
+                        x.test=X_test_label)
   Y_hat_bart_label = bart_obj_label$yhat.test.mean
+  error_bart_label = Y_test_label - Y_hat_bart_label
   
   
   error_ols_separate = c(error_ols_separate, error_ols_label)
@@ -77,10 +78,21 @@ methods = c(rep("ols", length(error_ols_whole)),
                  rep("bart", length(error_bart_whole)))
 errors_whole = c(error_ols_whole, error_bart_whole)
 Y_hat_whole = c(Y_hat_ols_whole, Y_hat_bart_whole)
-errors_whole = data.frame(methods=methods, Y_hat=Y_hat_whole, errors=errors_whole)
+errors_whole = data.frame(methods=methods, Y_hat=Y_hat_whole, 
+                          errors=errors_whole)
 
 data_frame_plots(errors_whole)
-(MSE_ols_whole = mean(error_ols_whole^2))
-(MSE_bart_whole = mean(error_bart_whole^2))
+(MSE_ols_whole)
+(MSE_bart_whole)
 
-# fitted with separated data
+# fitted with separate data
+methods = c(rep("ols", length(error_ols_separate)), 
+            rep("bart", length(error_bart_separate)))
+errors_separate = c(error_ols_separate, error_bart_separate)
+Y_hat_separate = c(Y_hat_ols_separate, Y_hat_bart_separate)
+errors_separate = data.frame(methods=methods, Y_hat=Y_hat_separate, 
+                             errors=errors_separate)
+
+data_frame_plots(errors_separate)
+(MSE_ols_separate)
+(MSE_bart_separate)
