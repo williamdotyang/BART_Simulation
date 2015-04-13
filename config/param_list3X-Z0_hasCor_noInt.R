0d1b803abd596ea4991dc8bf0d3d79cc375b727e
williamdotyang <williamdotyang@gmail.com>
2015-04-13 01:25:07 -0500

    configs for correlated covariates, no interaction between covariates and treatment label

---
+++ config/param_list3X-Z0_hasCor_noInt.R
@@ -0,0 +1,37 @@
+################################################################################
+## This file defines the default parameter values for simulation() in 
+## "src/simulation_1.R". See comments in "simulation_1.R" for description 
+## on meaning of each parameter.
+## Treatment Label: 0, number of covariates: 3
+################################################################################
+# treatment label
+TREATMENT_LABEL = 0
+# number of covariates, should be the same for all treatment_lable, 
+# only need to assign once
+n0 = 3 
+# number of observations
+N0 = 500 
+
+### make up intercept coefficients
+BETA0 = 0
+
+### make up coefficients for covariates x1,..., xn
+BETA1 = c(1, 1, 1)
+### make up 2nd order interaction index dataframe for x's, and their coefficients
+# INTER should be the same for all treatment_lable, only need to assign once
+INTER2 = data.frame(first=c(1,2,3), second=c(3,1,2))
+BETA2 = c(1, 1, 1)
+### make up 3nd order interaction index dataframe for x's, and their coefficients
+# INTER should be the same for all treatment_lable
+INTER3 = data.frame(first=1, second=2, third=3)
+BETA3 = 1
+
+### means
+# should be the same for all treatment_lable
+MUS = c(2, 2, 2)
+
+### specify the covariance matrix
+# should be the same for all treatment_lable, only need to assign once
+SIGMA = matrix(nrow=n0, c(1, 0.5, 0.5, 
+                          0.5, 1, 0.5, 
+                          0.5, 0.5, 1))
---
+++ config/param_list50X-Z1_hasCor_noInt.R
@@ -0,0 +1,35 @@
+################################################################################
+## This file defines the default parameter values for simulation() in 
+## "src/simulation_1.R". See comments in "simulation_1.R" for description 
+## on meaning of each parameter.
+## Treatment Label: 1, number of covariates: 50
+################################################################################
+# treatment label
+TREATMENT_LABEL = 1
+# number of covariates, should be the same for all treatment_lable, 
+# only need to assign once
+#n0 = 50
+# number of observations
+N0 = 1000
+
+### make up intercept coefficients
+BETA0 = 200
+
+### make up coefficients for covariates x1,..., xn
+BETA1 = rep(1, 50)
+### make up 2nd order interaction index dataframe for x's, and their coefficients
+# INTER should be the same for all treatment_lable, only need to assign once
+#INTER2 = choose2inter(n=n0, 10)
+BETA2 = rep(1, 10)
+### make up 3nd order interaction index dataframe for x's, and their coefficients
+# INTER should be the same for all treatment_lable, only need to assign once
+#INTER3 = choose3inter(n=n0, 5)
+BETA3 = rep(1, 5)
+
+### means
+# should be the same for all treatment_lable
+MUS = rep(2, n0)
+
+### specify the covariance matrix
+# should be the same for all treatment_lable, only need to assign once
+#SIGMA = matrix(rep(0.5, n0^2), nrow=n0, ncol=n0) + diag(0.5, n0)
